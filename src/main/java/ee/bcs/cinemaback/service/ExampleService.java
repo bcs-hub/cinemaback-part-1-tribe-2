85c916882c2a23b269546120e7292091733880ef
MeelisBCS <meelis.teern@bcs.ee>
2025-08-21 18:24:24 +0300

    Hello cat

---
+++ src/main/java/ee/bcs/cinemaback/service/ExampleService.java
@@ -0,0 +1,12 @@
+package ee.bcs.cinemaback.service;
+
+public class ExampleService {
+
+    public static void main(String[] args) {
+        printToConsole();
+    }
+
+    public static void printToConsole() {
+        System.out.println("Hello cat");
+    }
+}
