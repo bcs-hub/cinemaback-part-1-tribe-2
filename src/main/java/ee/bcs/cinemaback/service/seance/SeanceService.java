79a0a7322f36d7b81c9cd35f5975db9b154aff22
Karl Erik Põhako <est.karl@gmail.com>
2025-08-24 10:48:51 +0300

    CMI-9 improved.
    Added true seance time overlap check with room cleanup time into checkSeancesTimeAndRoom method.

--- src/main/java/ee/bcs/cinemaback/service/seance/SeanceService.java
+++ src/main/java/ee/bcs/cinemaback/service/seance/SeanceService.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.Instant;
 import java.util.ArrayList;
