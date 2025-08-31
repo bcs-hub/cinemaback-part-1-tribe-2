4267e17ab1a7bf052648173873763060d9208820
arq45 <reio.poder@gmail.com>
2025-08-31 15:51:47 +0300

    CMI-13 complete

--- src/main/java/ee/bcs/cinemaback/CinemaBackApplication.java
+++ src/main/java/ee/bcs/cinemaback/CinemaBackApplication.java
@@ -3,10 +3,12 @@ package ee.bcs.cinemaback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @SpringBootApplication
 @EnableCaching
+@EnableTransactionManagement
 public class CinemaBackApplication {
     public static void main(String[] args) {
         SpringApplication.run(CinemaBackApplication.class, args);
