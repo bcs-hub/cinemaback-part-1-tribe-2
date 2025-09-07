2fdef6994dbb2dfaeba83b1baaec91217537bc73
Karl Erik Põhako <est.karl@gmail.com>
2025-09-07 09:58:24 +0300

    CMI-13 complete

--- src/main/java/ee/bcs/cinemaback/controller/genre/GenreAdminController.java
+++ src/main/java/ee/bcs/cinemaback/controller/genre/GenreAdminController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
